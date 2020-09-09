ad4474dbc7577a373087cdf9415dc0a51d2812d0
ChristopherParga <38229095+ChristopherParga@users.noreply.github.com>
2020-09-08 19:43:55 -0500

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
 # define SIZE 30
 //Gustavo Paez Villalobos A01039751
 // Rafa Mtz
-// Christopher Parga
+// Christopher Parga A00818942
 // Grupo 3
 
 
-int main() {
-  printf("Hello World");
+static volatile sig_atomic_t keep_running = 1;
+
+static void sig_handler(int _){
+    puts("Handler.");
+    keep_running = 1;
+}
+
+int main(void){
+    signal(SIGINT, sig_handler);
+
+    while (keep_running)
+        puts("Corriendo...");
+
+    
+    return 0;
 }
